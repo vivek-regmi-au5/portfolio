4e77766817346ff09b63999e90f780690c13bc20
Vivek Regmi <vivekregmiiitg@gmail.com>
2019-04-19 02:26:14 +0530

    Jobs model and views created

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Blog(models.Model):
+	title = models.CharField(max_length=30)
 	image = models.ImageField(upload_to='images/')
-	first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
-	summary = models.CharField(max_length=200)
+	pub_date = models.DateTimeField()
+	body = models.TextField()
